af5db4d97f04548f36592e6a1e87d8c7c3f4a077
installer <installer@gmail.com>
2025-06-27 17:08:11 -0400

    remove chaotic nyx

--- flake.nix
+++ flake.nix
@@ -1,7 +1,6 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    chaotic.url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
     nix-gaming.url = "github:fufexan/nix-gaming";
   };
 
@@ -11,7 +10,6 @@
       specialArgs = { inherit inputs; };
       modules = [
         ./configuration.nix
-        inputs.chaotic.nixosModules.default
       ];
     };
   };
