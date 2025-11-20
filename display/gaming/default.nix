10730b29534e0a9104a385cf5df5d31bbfd7af71
installer <installer@gmail.com>
2025-11-20 13:37:41 -0500

    update

--- display/gaming/default.nix
+++ display/gaming/default.nix
@@ -14,14 +14,12 @@
 
   environment.systemPackages = with pkgs; [
     inputs.nix-gaming.packages.${pkgs.system}.osu-stable
-    (inputs.nix-gaming.packages.${pkgs.system}.osu-lazer-bin.override {
-      pipewire_latency = "32/48000";
-    })
     heroic
+    osu-lazer-bin
     #(prismlauncher.override {
     #  textToSpeechSupport = false;
     #})
     #bolt-launcher
-    dolphin-emu
+    #dolphin-emu
   ];
 }
