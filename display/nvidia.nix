654247e80939ad5d4a4b3547dba932d4be7d75d3
installer <installer@gmail.com>
2025-11-20 13:37:36 -0500

    update

--- display/nvidia.nix
+++ display/nvidia.nix
@@ -1,14 +1,19 @@
 # 💫 https://github.com/JaKooLit 💫 #
 
-{ lib, pkgs, config, ... }:
+{
+  lib,
+  pkgs,
+  config,
+  ...
+}:
 with lib;
 {
-  boot.kernelParams = [ 
-    "nvidia_drm.fbdev=1" 
+  boot.kernelParams = [
+    "nvidia_drm.fbdev=1"
     "nvidia.NVreg_RegistryDwords=\"PowerMizerEnable=0x1; PerfLevelSrc=0x2222; PowerMizerLevel=0x3; PowerMizerDefault=0x3; PowerMizerDefaultAC=0x3\""
   ];
-  boot.initrd.kernelModules = [ 
-    "nvidia" 
+  boot.initrd.kernelModules = [
+    "nvidia"
     "nvidia_modeset"
     "nvidia_uvm"
     "nvidia_drm"
@@ -16,30 +21,30 @@ with lib;
   ];
   boot.blacklistedKernelModules = [ "nouveau" ];
   services.xserver.videoDrivers = [ "nvidia" ];
-  
+
   hardware.graphics.enable = true;
   hardware.graphics.extraPackages = with pkgs; [
-    vaapiVdpau
+    libva-vdpau-driver
     libvdpau
-    libvdpau-va-gl 
+    libvdpau-va-gl
     nvidia-vaapi-driver
     vdpauinfo
     libva
-    libva-utils	
+    libva-utils
     rocmPackages.clr
   ];
 
   hardware.nvidia = {
     # Modesetting is required.
     modesetting.enable = true;
-      
+
     # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
     powerManagement.enable = false;
-      
+
     # Fine-grained power management. Turns off GPU when not in use.
     # Experimental and only works on modern Nvidia GPUs (Turing or newer).
     powerManagement.finegrained = false;
-      
+
     dynamicBoost.enable = false; # Dynamic Boost
 
     nvidiaPersistenced = false;
@@ -52,11 +57,11 @@ with lib;
     # Only available from driver 515.43.04+
     # Currently alpha-quality/buggy, so false is currently the recommended setting.
     open = false;
-      
+
     # Enable the Nvidia settings menu,
     # accessible via `nvidia-settings`.
     nvidiaSettings = false;
-      
+
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
     package = config.boot.kernelPackages.nvidiaPackages.stable;
 
